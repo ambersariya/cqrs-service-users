bc17a18cc38a8c232ce6747d8a2e491ba8b047f6
Danish Javed <danish.javed@mwrinfosecurity.com>
2018-05-30 14:39:42 +0100

    reshaped our directory structure

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Prooph\Bundle\EventStore\ProophEventStoreBundle::class => ['all' => true],
+    Prooph\Bundle\ServiceBus\ProophServiceBusBundle::class => ['all' => true],
 ];
