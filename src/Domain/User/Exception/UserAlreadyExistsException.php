7c21925d027936a9bbcdefb711ab80d2f20b1d6b
Danish Javed <danish.javed@mwrinfosecurity.com>
2018-06-01 16:58:43 +0100

    removed comment

--- src/Domain/User/Exception/UserAlreadyExistsException.php
+++ src/Domain/User/Exception/UserAlreadyExistsException.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Danish Javed
- * Date: 31/05/2018
- * Time: 10:28
- */
 
 namespace App\Domain\User\Exception;
-
 
 class UserAlreadyExistsException extends \RuntimeException
 {
